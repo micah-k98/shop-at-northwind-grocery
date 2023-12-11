19b871c64f3a41015ceb9929113117a1a9975a05
micah-k98 <146677653+micah-k98@users.noreply.github.com>
2023-12-11 10:59:55 -0700

    Updates

--- scripts/details.js
+++ scripts/details.js
@@ -21,23 +21,26 @@ async function displayDetails() {
     let id = -1;
     if (urlParams.has("id") == true) {
         id = urlParams.get("id");
-    }
-    let product = await filterDataService.product(id)
-    
-    let categories = await allDataService.categoriesData();
-    let productCategory;
-    categories.forEach(category => {
-        if (category.id == product.categoryId) productCategory = category.name;
-    })
-    
-    productDetails.innerText = "";
-    let card = productDetailsTemplate.content.cloneNode(true);
-    
-    card.getElementById("productName").innerText = product.productName;
-    card.getElementById("productCategory").innerText = productCategory;
-    card.getElementById("productPrice").innerText = product.unitPrice;
-    card.getElementById("productQty").innerText = product.unitsInStock;
-    card.getElementById("productSupplier").innerText = product.supplier;
 
-    productDetails.appendChild(card);
+        let product = await filterDataService.product(id)
+    
+        let categories = await allDataService.categoriesData();
+        let productCategory;
+        categories.forEach(category => {
+            if (category.id == product.categoryId) productCategory = category.name;
+        })
+        
+        productDetails.innerText = "";
+        let card = productDetailsTemplate.content.cloneNode(true);
+        
+        card.getElementById("productName").innerText = product.productName;
+        card.getElementById("productCategory").innerText = productCategory;
+        card.getElementById("productPrice").innerText = product.unitPrice;
+        card.getElementById("productQty").innerText = product.unitsInStock;
+        card.getElementById("productSupplier").innerText = product.supplier;
+
+        productDetails.appendChild(card);
+    }
+    else location.href = "/products.html";
+    
 }
